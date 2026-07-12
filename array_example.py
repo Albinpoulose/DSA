61d23a2094e8ab5645c333afbd44ca9883081a02
Albin <albinfreesme@gmail.com>
2026-07-12 18:06:57 +0530

    array

---
+++ array_example.py
@@ -0,0 +1,20 @@
+# import array 
+
+from array import *
+
+val = array('i',[1,2,3,4,5,6])
+
+for i in range(0,6):
+    print(val[i],end=" ")
+
+print('\n')
+for x in val :
+    print (x,end=",")
+
+print('\n')
+for i in range(0,len(val)):
+    print(val[i],end=" ")
+
+
+
+
